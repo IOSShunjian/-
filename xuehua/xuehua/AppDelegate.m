3719c2f282d302ffeb54ad7405a95bd4c2c085a1
ios <528392911@qq.com>
2016-09-07 19:46:53 +0800

    完成核心代码

--- xuehua/xuehua/AppDelegate.m
+++ xuehua/xuehua/AppDelegate.m
@@ -2,8 +2,6 @@
 //  AppDelegate.m
 //  xuehua
 //
-//  Created by 崔顺建 on 16/9/7.
-//  Copyright © 2016年 cui. All rights reserved.
 //
 
 #import "AppDelegate.h"
--- xuehua/xuehua/ViewController.m
+++ xuehua/xuehua/ViewController.m
@@ -2,26 +2,63 @@
 //  ViewController.m
 //  xuehua
 //
-//  Created by 崔顺建 on 16/9/7.
-//  Copyright © 2016年 cui. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) CADisplayLink* link;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //    self.view.layer.contents = (__bridge id _Nullable)([UIImage imageNamed:@"snowbg"].CGImage);
+    
+    UIImageView* bgImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"snowbg"]];
+    bgImage.frame = self.view.bounds;
+    [self.view addSubview:bgImage];
+    
+    CADisplayLink* link = [CADisplayLink displayLinkWithTarget:self selector:@selector(autoFall)];
+    [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+    
+    self.link = link;
+}
+//点击开始 点击暂停
+- (void)touchesBegan:(NSSet<UITouch*>*)touches withEvent:(UIEvent*)event
+{
+    self.link.paused = !self.link.paused;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)autoFall
+{
+    UIImageView* image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"雪花"]];
+    
+    //产生图片缩放 注意值是小数 接收时 使用float
+    float ang = arc4random_uniform(50) / 100.0;
+    image.transform = CGAffineTransformMakeScale(ang, ang);
+    
+    //宽度随机产生
+    CGFloat imgX = arc4random_uniform(self.view.bounds.size.width);
+    CGPoint imagePoint = CGPointMake(imgX, -10);
+    image.center = imagePoint;
+    [self.view addSubview:image];
+    
+    //动画改变frame  8秒执行y值的改变
+    [UIView animateWithDuration:8.0f animations:^{
+        //图片选中
+        image.transform = CGAffineTransformRotate(image.transform, M_PI);
+        
+        image.center = CGPointMake(imgX, self.view.bounds.size.height + 10);
+        image.alpha = 0.1;
+    }
+                     completion:^(BOOL finished) {
+                         [image removeFromSuperview];
+                     }];
+    
+    // NSLog(@"%@", NSStringFromCGRect(image.frame));
 }
+
 
 @end
